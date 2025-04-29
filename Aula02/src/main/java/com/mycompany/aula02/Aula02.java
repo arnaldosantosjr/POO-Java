04e29ab2da02b41bab557d41abed2bd1d431c0ee
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-04-29 20:30:35 -0300

    Cerate Aula02
    Classe caneta.

---
+++ Aula02/src/main/java/com/mycompany/aula02/Aula02.java
@@ -0,0 +1,23 @@
+package com.mycompany.aula02;
+public class Aula02 {
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic";
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.carga = 100;
+        c1.destampar();
+        c1.status();
+        c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Compactor";
+        c2.cor = "Preta";
+        c2.ponta = 0.7f;
+        c2.carga = 80;
+        c2.tampar();
+        c2.status();
+        c2.rabiscar();
+    }
+}
