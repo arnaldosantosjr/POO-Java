afe20905da1fec68447428d1bf48fb517da87845
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-04-30 09:40:45 -0300

    Updade Aula02b.java
    Inserindo Carro 2

--- Aula02b/src/main/java/com/mycompany/aula02b/Aula02b.java
+++ Aula02b/src/main/java/com/mycompany/aula02b/Aula02b.java
@@ -4,7 +4,7 @@ public class Aula02b {
     public static void main(String[] args) {
         Carro c1 = new Carro();
         c1.fabricante = "Ford";
-        c1.modelo = "Sedan";
+        c1.modelo = "Gol";
         c1.cor = "Preto";
         c1.freio = "ABS";
         c1.motor = 1.0f;
@@ -14,5 +14,23 @@ public class Aula02b {
         
         c1.status();
         c1.abastecer();
+        
+        Carro c2 = new Carro();
+        c2.fabricante = "Chevrolet";
+        c2.modelo = "Onix";
+        c2.cor = "Branco";
+        c2.freio = "ABS";
+        c2.motor = 1.0f;
+        c2.ano = 2020;
+        c2.vendido = false;
+        c2.abastecido = true;
+
+        c2.status();
+        c2.abastecer();
+        c2.tanquevazio();
+
+    
     }
+
+
 }
