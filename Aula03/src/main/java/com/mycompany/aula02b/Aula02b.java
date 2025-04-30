026246b65ef2e6348145eb8967ca3fe2839df77b
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-04-30 12:56:49 -0300

    Create Aula03.java
    Alterando a visibilidade de um objeto

---
+++ Aula03/src/main/java/com/mycompany/aula02b/Aula02b.java
@@ -0,0 +1,13 @@
+package com.mycompany.aula02b;
+public class Aula02b {
+
+    public static void main(String[] args) {
+        Carro c1 = new Carro();
+        c1.status();
+       
+
+    
+    }
+
+
+}
