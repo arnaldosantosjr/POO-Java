5d131af6ab90d0695c565d4b3eef0cf436d79134
arnaldojrsantos <arnaldosantosjr01@gmail.com>
2025-05-02 16:22:29 -0300

    create Aula04bbb
    Praticando no objeto carro

---
+++ Aula04bbb/src/main/java/com/mycompany/aula04bbb/Aula04bbb.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula04bbb;
+
+/**
+ *
+ * @author admin
+ */
+public class Aula04bbb {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Aula04bbb/src/main/java/com/mycompany/aula04bbb/Carro.java
@@ -0,0 +1,9 @@
+package com.mycompany.aula04bbb;
+public class Carro {
+    private String fabricante;
+    private String veiculo;
+    protected String cor;
+    
+    
+    
+}
